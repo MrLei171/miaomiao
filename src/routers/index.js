264ee9912eb13a0f4267220ee6f750f7286da7cf
malei <ml123456>
2021-02-27 21:33:26 +0800

    2/27

---
+++ src/routers/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import movieRouter from './movie'
+import cinemaRouter from './cinema'
+import mineRouter from './mine'
+Vue.use(VueRouter)
+
+
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes:[
+    movieRouter,
+    cinemaRouter,
+    mineRouter
+  ]
+})
+
+export default router
---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Header/>
+        cinema
+        <TabBar/>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import TabBar from '@/components/TabBar'
+export default {
+    name:'Cinema',
+    components:{
+        Header,
+        TabBar
+    }
+}
+</script>
